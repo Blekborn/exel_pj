d81a68d862f55a938ca9a32f0552a04aa91d8e7e
Blekborn <dmytrodatsenko10@gmail.com>
2021-08-20 14:05:00 +0300

    commit plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 module.exports = {
@@ -11,6 +12,13 @@ module.exports = {
     output: {
         filename: "bundle.[hash].js",
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname,'src'),
+            '@core': path.resolve(__dirname,'src/core')
+        }
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -23,7 +31,10 @@ module.exports = {
                     from: path.resolve(__dirname, 'src/favicon.ico'),
                     to: path.resolve(__dirname, 'dist')
                 }
-            ],
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css'
         })
     ]
 }
