f6a204007ab305429a70e49bc5cbfc437e608369
Blekborn <dmytrodatsenko10@gmail.com>
2021-08-20 08:28:30 +0300

    commit html clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: "development",
@@ -7,5 +8,11 @@ module.exports = {
     output: {
         filename: "bundle.[hash].js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: "index.html"
+        })
+    ]
 }
